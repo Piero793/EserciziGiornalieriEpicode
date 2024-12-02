1ebeaa4cd17b19983673a2be9f5667380c6e73c0
“Piero793” <pierolacitignola7@gmail.com>
2024-12-02 17:45:51 +0100

    02/12

--- esercizio02-12/D1.js
+++ esercizio02-12/D1.js
@@ -88,10 +88,13 @@ let name1 ="john";
 let name2 ="John";
 console.log(name1 != name2);
 
-if (name2 ="john") {
-  console.log(name1 == name2);
-}
+console.log(name1.toLowerCase() === name2.toLowerCase());
 
-if (name1 && name2) {
-  console.log(name1==name2);
-}
+
+
+
+
+
+
+
+
