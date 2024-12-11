7aff430a2d26a270dea94d27910ea173afae558a
“Piero793” <pierolacitignola7@gmail.com>
2024-12-11 14:55:14 +0100

    commit11/12

---
+++ esercizio11-12/script.js
@@ -0,0 +1,30 @@
+document.addEventListener('DOMContentLoaded', (event) => {
+    const newTaskInput = document.getElementById('new-task-input');
+    const addTaskButton = document.getElementById('add-task-button');
+    const taskList = document.getElementById('task-list');
+  
+    addTaskButton.addEventListener('click', () => {
+      const taskText = newTaskInput.value;
+      if (taskText.trim()) {
+        const newTask = document.createElement('li');
+        newTask.textContent = taskText;
+  
+        // Bottone per eliminare il task
+        const deleteButton = document.createElement('button');
+        deleteButton.textContent = 'Elimina';
+        deleteButton.style.marginLeft = '10px';
+        deleteButton.addEventListener('click', () => {
+          taskList.removeChild(newTask);
+        });
+  
+        newTask.appendChild(deleteButton);
+        newTask.addEventListener('click', () => {
+          newTask.classList.toggle('completed');
+        });
+  
+        taskList.appendChild(newTask);
+        newTaskInput.value = '';
+      }
+    });
+  });
+  
