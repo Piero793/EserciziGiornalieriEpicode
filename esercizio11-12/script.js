30edbc8438185c446a1b60a296214cc9a8db4bf7
“Piero793” <pierolacitignola7@gmail.com>
2024-12-11 17:25:10 +0100

    commit

--- esercizio11-12/script.js
+++ esercizio11-12/script.js
@@ -1,33 +1,34 @@
 
 
-document.addEventListener('DOMContentLoaded', (e) => {
-    const newTaskInput = document.querySelector('#new-task-input');
-    const addTaskButton = document.querySelector('#add-task-button');
-    const taskList = document.querySelector('#task-list');
 
-    addTaskButton.addEventListener('click', () => {
-        const taskText = newTaskInput.value;
-        if (taskText.trim()) {
-            const newTask = document.createElement('li');
-            newTask.textContent = taskText;
-          
-            // Bottone per eliminare il task
-            const deleteButton = document.createElement('button');
-            deleteButton.textContent = 'Elimina';
-            deleteButton.style.marginLeft = '10px';
-            deleteButton.style.backgroundColor = "red";
-            deleteButton.addEventListener('click', () => {
-                taskList.removeChild(newTask);
-            });
+const newTaskInput = document.querySelector('#new-task-input');
+const addTaskButton = document.querySelector('#add-task-button');
+const taskList = document.querySelector('#task-list');
 
-            newTask.appendChild(deleteButton);
-            newTask.addEventListener('click', () => {
-                newTask.classList.toggle('completed');
-            });
 
-            taskList.appendChild(newTask);
-            newTaskInput.value = '';
-        }
-    });
+
+addTaskButton.addEventListener('click', () => {
+    const taskText = newTaskInput.value;
+    if (taskText.trim()) {
+        const newTask = document.createElement('li');
+        newTask.textContent = taskText;
+
+        // Bottone per eliminare il task
+        const deleteButton = document.createElement('button');
+        deleteButton.textContent = 'Elimina';
+        deleteButton.style.marginLeft = '10px';
+        deleteButton.style.backgroundColor = "red";
+        deleteButton.addEventListener('click', () => {
+            taskList.removeChild(newTask);
+        });
+
+        newTask.appendChild(deleteButton);
+        newTask.addEventListener('click', () => {
+            newTask.classList.toggle('completed');
+        });
+
+        taskList.appendChild(newTask);
+        newTaskInput.value = '';
+    }
 });
- 
+
