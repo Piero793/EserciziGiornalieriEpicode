6d1408cef0c495667935b4e3a64d80782f69dc5e
“Piero793” <pierolacitignola7@gmail.com>
2024-12-11 15:25:29 +0100

    commit11-12

--- esercizio11-12/script.js
+++ esercizio11-12/script.js
@@ -1,16 +1,16 @@
 
 
 document.addEventListener('DOMContentLoaded', (e) => {
-    const newTaskInput = document.getElementById('new-task-input');
-    const addTaskButton = document.getElementById('add-task-button');
-    const taskList = document.getElementById('task-list');
+    const newTaskInput = document.querySelector('#new-task-input');
+    const addTaskButton = document.querySelector('#add-task-button');
+    const taskList = document.querySelector('#task-list');
 
     addTaskButton.addEventListener('click', () => {
         const taskText = newTaskInput.value;
         if (taskText.trim()) {
             const newTask = document.createElement('li');
             newTask.textContent = taskText;
-
+          
             // Bottone per eliminare il task
             const deleteButton = document.createElement('button');
             deleteButton.textContent = 'Elimina';
@@ -30,3 +30,4 @@ document.addEventListener('DOMContentLoaded', (e) => {
         }
     });
 });
+ 
