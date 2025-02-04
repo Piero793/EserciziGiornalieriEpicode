f251d804690df8252251f662c9a591842b546fea
“Piero793” <pierolacitignola7@gmail.com>
2025-02-04 17:02:58 +0100

    euros

--- esercizio04-02/src/assets/components/AllTheBooks.jsx
+++ esercizio04-02/src/assets/components/AllTheBooks.jsx
@@ -1,18 +1,20 @@
 import { Component } from "react";
 import horror from "../../../data/horror.json";
-import { Button, Card, Container, Row, Col } from "react-bootstrap";
+import { Button, Card, Container, Row, Col, Badge } from "react-bootstrap";
 
 class AllTheBooks extends Component {
   render() {
     return (
       <Container>
         <Row>
-          {horror.map((films) => (
-            <Col lg={3} className="text-center" key={`films-${films.asin}`}>
+          {horror.map((books) => (
+            <Col lg={2} className="text-center" key={`films-${books.asin}`}>
               <Card>
-                <Card.Img variant="top" src={films.img} />
+                <Card.Img variant="top" src={books.img} />
                 <Card.Body>
-                  <Card.Title>{films.Title}</Card.Title>
+                  <Card.Title>{books.Title}</Card.Title>
+                  <Badge bg="secondary me-3">{books.price}€</Badge>
+
                   <Button variant="primary">buy</Button>
                 </Card.Body>
               </Card>
